8a23562eceb33f4321ffc70ef9e6838702423be9
Hosein-Joleini <hosseinjoleini94@gmail.com>
2023-09-07 19:18:15 +0330

    Finish Building Super Quality Section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -7,10 +7,10 @@ const PopularProducts = () => {
       className="max-container w-full flex flex-col items-start"
       id="products"
     >
-      <h2 className="font-bold font-palanquin text-4xl mb-6">
+      <h2 className="font-bold font-palanquin text-4xl mb-6 leading-[62px]">
         Our <span className="text-coral-red">Popular</span> Products
       </h2>
-      <p className="text-slate-gray font-montserrat mb-16">
+      <p className="text-slate-gray font-montserrat mb-16 max-w-lg leading-normal">
         Experience top-notch quality and style with our sought-after selections.
         Discover a world of comfort, design, and value
       </p>
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,32 @@
+import Button from "../components/Button";
+
+import { shoe8 } from "../assets/images";
+
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section className="max-container w-full flex max-lg:flex-col gap-12 items-center">
+      <div className="flex-1">
+        <h2 className="font-bold font-palanquin text-4xl leading-[62px] mb-4">
+          We Provide You <span className="text-coral-red">Super Quality</span>{" "}
+          Shoes
+        </h2>
+        <p className="text-slate-gray text-lg font-montserrat mb-6 leading-7 max-w-lg">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+
+        <p className="text-slate-gray text-lg  font-montserrat leading-7 mb-10 max-w-lg">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+
+        <Button label="View details" />
+      </div>
+      <div className="flex-1">
+        <img src={shoe8} alt="Nike Shoe" width="570" height="522" />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
