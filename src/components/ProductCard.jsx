cbb8833f33e5d7a04fd3cb960fefd10d3f5b9e84
Hosein-Joleini <hosseinjoleini94@gmail.com>
2023-09-07 18:58:00 +0330

    Finish Popular Products Section

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,21 @@
+import { star } from "../assets/icons";
+
+const ProductCard = ({ name, imgURL, price }) => {
+  return (
+    <div className="flex-1 flex flex-col items-start">
+      <img src={imgURL} alt={name} />
+      <div className="flex items-center gap-2 mt-10 mb-4">
+        <img src={star} alt="star icon" />
+        <p className="text-xl text-slate-gray font-montserrat">(4.5)</p>
+      </div>
+      <p className="font-semibold whitespace-nowrap text-2xl font-palanquin mb-4">
+        {name}
+      </p>
+      <p className="text-coral-red text-xl font-semibold font-montserrat">
+        {price}
+      </p>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,5 +1,31 @@
+import ProductCard from "../components/ProductCard";
+import { products } from "../constants";
+
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  return (
+    <section
+      className="max-container w-full flex flex-col items-start"
+      id="products"
+    >
+      <h2 className="font-bold font-palanquin text-4xl mb-6">
+        Our <span className="text-coral-red">Popular</span> Products
+      </h2>
+      <p className="text-slate-gray font-montserrat mb-16">
+        Experience top-notch quality and style with our sought-after selections.
+        Discover a world of comfort, design, and value
+      </p>
+      <div className="flex-1 flex max-lg:flex-wrap lg:gap-6 gap-16 max-sm:flex-col max-sm:w-full">
+        {products.map((item) => (
+          <ProductCard
+            key={item.name}
+            name={item.name}
+            price={item.price}
+            imgURL={item.imgURL}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default PopularProducts;
