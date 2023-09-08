8e2ed10dae72134b834688b6695f1d622d9980ab
Hosein-Joleini <hosseinjoleini94@gmail.com>
2023-09-08 14:54:44 +0330

    Finish Building Subscribe Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,12 @@
-const Button = ({ label, iconURL, background }) => {
+const Button = ({ label, iconURL, background, className }) => {
   return (
     <button
       type="button"
-      className={`flex items-center gap-4 py-4 px-8 rounded-full font-montserrat text-lg ${
+      className={`flex items-center justify-center gap-4 py-4 px-8 rounded-full font-montserrat text-lg ${
         background
           ? "bg-white-400 text-slate-gray border border-slate-gray"
-          : "text-white-400 bg-coral-red border border-coral-red"
-      }`}
+          : "text-white bg-coral-red border border-coral-red"
+      } ${className}`}
     >
       {label}
       {iconURL && <img src={iconURL} alt="arrow right icon" />}
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,5 +1,21 @@
+import Button from "../components/Button";
+
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section className="max-contianer w-full flex max-lg:flex-col lg:gap-12 lg:items-center gap-4 lg:justify-center">
+      <h2 className="flex-1 font-bold font-palanquin text-4xl mb-6 leading-[62px]">
+        Sign Up for <span className="text-coral-red">Updates</span> & Newsletter
+      </h2>
+      <form className="flex-1 flex max-sm:flex-col max-sm:gap-4 relative">
+        <input
+          type="email"
+          placeholder="subscribe@nike.com"
+          className="input"
+        />
+        <Button label="Sign Up" className="sm:absolute sm:right-2 top-1" />
+      </form>
+    </section>
+  );
 };
 
 export default Subscribe;
